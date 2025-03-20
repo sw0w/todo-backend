670e5301ee9b4325294bbb9a1746fdd947067731
sw0w <makelajasu@gmail.com>
2025-03-20 15:01:35 +0200

    test

--- backend/server/app.test.ts
+++ backend/server/app.test.ts
@@ -1,16 +1,8 @@
 import request from "supertest";
 import app from "./app";
-import { describe, test, expect, beforeAll } from "vitest";
-import User from "./models/user";
+import { describe, test, expect } from "vitest";
 
 describe("app tests", () => {
-  beforeAll(async () => {
-    const existingUser = await User.findOne({ username: "testuser" });
-    if (existingUser) {
-      await User.deleteOne({ username: "testuser" });
-    }
-  });
-
   test("unauthorized route, should return 404", async () => {
     const res = await request(app).get("/todos");
     expect(res.status).toBe(404);
@@ -19,7 +11,7 @@ describe("app tests", () => {
   test("should register a user", async () => {
     const res = await request(app)
       .post("/register")
-      .send({ username: "testuser", password: "123456" });
+      .send({ username: "3243322323", password: "123456" });
     expect(res.status).toBe(201);
   });
 });
